35b35bd59d54d3511becb2d07db84d183de55979
Etienne Petrilli <etienne.petrilli@c1r15s4.marseille.42.school>
2025-07-24 15:53:32 +0200

    Main0Corrige

--- main0.c
+++ main0.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main0.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etienne.petrilli <etienne.petrilli@learne  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 18:31:01 by etienne.petri     #+#    #+#             */
-/*   Updated: 2025/07/24 15:23:48 by etienne.petri    ###   ########.fr       */
+/*   Updated: 2025/07/24 15:52:42 by etienne.petri    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,42 @@
 #include "../corretienne/ex3/ft_print_numbers.c"
 #include "../corretienne/ex4/ft_is_negative.c"
 
-void	ft_putchar(char c)
+void	ft_putcharcusto(char c)
 {
 	write(1, &c, 1);
 }
 
 void	ft_putstr(char *c)
 {
-	while (c)
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
 	{
-		ft_putchar(c);
-		c++;
+		ft_putcharcusto(c[i]);
+		i++;
 	}
 }
 
-void	main(void)
+int	main(void)
 {
-	ft_putstr("----------Test ft_putchar----------\n");
+	ft_putstr("----------Test ft_putchar----------\n\n");
 	ft_putstr("ft_putchar('c') : \"");
 	ft_putchar('c');
 	ft_putstr("\"\n\n");
-	ft_putstr("----------Test ft_print_alphabet----------\n");
+	ft_putstr("----------Test ft_print_alphabet----------\n\n");
 	ft_putstr("ft_print_alphabet() : \"");
 	ft_print_alphabet();
 	ft_putstr("\"\n\n");
-        ft_putstr("----------Test ft_print_reverse_alphabet----------\n");
+        ft_putstr("----------Test ft_print_reverse_alphabet----------\n\n");
         ft_putstr("ft_print_reverse_alphabet() : \"");
 	ft_print_reverse_alphabet();
 	ft_putstr("\"\n\n");
-	ft_putstr("----------Test ft_print_alphabet----------\n");
+	ft_putstr("----------Test ft_print_numbers----------\n\n");
         ft_putstr("ft_print_numbers() : \"");
 	ft_print_numbers();
 	ft_putstr("\"\n\n");
-        ft_putstr("----------Test ft_print_alphabet----------\n");
+        ft_putstr("----------Test ft_is_negative----------\n\n");
         ft_putstr("ft_is_negative(-6) : \"");
 	ft_is_negative(-6);
 	ft_putstr("\"\n");
@@ -57,5 +60,7 @@ void	main(void)
         ft_putstr("\"\n");
         ft_putstr("ft_is_negative(0) : \"");
         ft_is_negative(0);
-        ft_putstr("\"\n");
+        ft_putstr("\"\n\n----------FIN----------\n");
+	
+	return (0);
 }
